d412d201a6f0741998a1b52f59aee3c05ec1e1ea
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2024-12-21 01:07:59 +0100

    wdapq

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ const CallScreen = () => {
 
         incomingCall.on('stream', (remoteStream) => {
           remoteAudioRef.current.srcObject = remoteStream;
+          remoteAudioRef.current.volume = 2.0; // Amplify the remote stream volume
           setCallStatus('In Call');
         });
       });
